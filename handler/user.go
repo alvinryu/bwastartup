f095676d9fbd916a84277a8d529da0a6c2518209
alvinrg <alvinrg12@gmail.com>
2020-11-18 16:06:30 +0700

    jwt auth generate token

--- handler/user.go
+++ handler/user.go
@@ -4,16 +4,18 @@ import (
 	"fmt"
 	"bwastartup/user"
 	"bwastartup/helper"
+	"bwastartup/auth"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 type userHandler struct {
 	userService user.Service
+	authService auth.Service
 }
 
-func NewUserHandler(userService user.Service) *userHandler {
-	return &userHandler{userService}
+func NewUserHandler(userService user.Service, authService auth.Service) *userHandler {
+	return &userHandler{userService, authService}
 }
 
 func (h *userHandler) RegisterUser(c *gin.Context){
@@ -36,8 +38,13 @@ func (h *userHandler) RegisterUser(c *gin.Context){
 		return
 	}
 
-	//token, err := h.jwtService.GenerateToken()
-	formatter := user.FormatUser(newUser, "tokentokentokentoken")
+	token, err := h.authService.GenerateToken(newUser.ID)
+	if err != nil {
+		response := helper.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	formatter := user.FormatUser(newUser, token)
 
 	response := helper.APIResponse("Account has ben registered", http.StatusOK, "success", formatter)
 	c.JSON(http.StatusOK, response)
@@ -65,7 +72,14 @@ func (h *userHandler) Login(c *gin.Context){
 		return
 	}
 
-	formatter := user.FormatUser(loggedinUser, "tokentokentokentoken")
+	token, err := h.authService.GenerateToken(loggedinUser.ID)
+	if err != nil {
+		response := helper.APIResponse("Login failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	formatter := user.FormatUser(loggedinUser, token)
+	
 	response := helper.APIResponse("Succesfully login", http.StatusOK, "success", formatter)
 	c.JSON(http.StatusOK, response)
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
   "bwastartup/user"
   "bwastartup/handler"
+  "bwastartup/auth"
   "log"
   "github.com/gin-gonic/gin"
   "gorm.io/driver/mysql"
@@ -19,8 +20,9 @@ func main() {
 
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
+	authService := auth.NewService()
 
-	userHandler := handler.NewUserHandler(userService)
+	userHandler := handler.NewUserHandler(userService, authService)
 	router := gin.Default()
 	api := router.Group("/api/v1")
 
